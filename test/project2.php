fbade8006a73bf9a7c3bceab2175286a00e85bab
Евгений Олейников < >
2017-01-23 18:51:05 +0300

    up

--- test/project2.php
+++ test/project2.php
@@ -4,18 +4,35 @@
 function stroki () {
     $s='abdfabg';
     echo $s;
-    $sm=$s[0]+$s[1];
+    $sm=$s[0];
+    $sm+=$s[1];
+
     return $sm;
 
 }
 
-    function bd(){
-        $host='127.0.0.1'; // имя хоста (уточняется у провайдера)
-        $database='n_today'; // имя базы данных, которую вы должны создать
+    function bd_connect(){
+        $host='localhost'; // имя хоста (уточняется у провайдера)
+        $database='n_today'; // имя базы данных, которую вы должны создать n_today
         $user='root'; // заданное вами имя пользователя, либо определенное провайдером
-        $pswd='admin'; // заданный вами пароль
-        $dbh = mysqli_connect($host,$user,$pswd,$database);
+        $paswd='admin'; // заданный вами пароль
+        $link = mysqli_connect($host,$user,$paswd,$database);
+        mysqli_query($link, 'set names cp1251');
     }
 
+    function xml1(){
+        $dom = new DomDocument('1.0');
+        $goods = $dom->appendChild($dom->createElement('Товары'));
+        $good=$goods->appendChild($dom->createElement('Товар'));
+        $cen=$good->appendChild($dom->createElement('Цена'));
+        $cen2=$good->appendChild($dom->createElement('Цена'));
+        $options=$good->appendChild($dom->createElement('Свойства'));
+        $raz=$good->appendChild($dom->createElement('Разделы'));
+        $dom->formatOutput = true;
+        $test1 = $dom->saveXML();
+        $dom->save('test1.xml');
+
+    }
 echo stroki();
-bd();
+//bd();
+xml1();
