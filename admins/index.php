26e6e9f49d1149a7c29d75651b8239d2e1a8d6ad
evgenii0016 <evgenii.oleinikov@mail.ru>
2017-02-02 22:57:41 +0300

    update

--- admins/index.php
+++ admins/index.php
@@ -5,8 +5,15 @@ $APPLICATION->SetTitle("Администратирование");
 <html>
 	<body>
 
-		<p><a href="/admins/users.php"> Список пользователей</a></p> </br>
- 		<p><a href="/admins/users24.php"> Список пользователей зарегистрированных за последние 24 часа</a></p>
+		<p><a href="/admins/users.php" id="some_link"> Список пользователей</a></p> </br>
+ 		<p><a href="/admins/users24.php" id="some_link"> Список пользователей зарегистрированных за последние 24 часа</a></p>
+
+	<script>
+		var myLink = document.getElementById('some_link');
+		myLink.onclick = function(){
+			myLink.style.backgroundColor = '#c2c2a3';
+		}
+	</script>
 
 	</body>
 </html>
